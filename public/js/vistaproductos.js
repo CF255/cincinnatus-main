8c097ea5638b59e39040fb59b257014cbba2a3cd
CF255 <andrewsluis2011@gmail.com>
2023-04-11 23:04:00 -0400

    hn

---
+++ public/js/vistaproductos.js
@@ -0,0 +1,37 @@
+
+/* ocultar menu imputs y sidemenu rigth */
+const check = document.querySelector('#check');
+const tablaproductos = document.getElementById('tablaproductos');
+
+
+check.addEventListener('click', e =>{
+    tablaproductos.classList.toggle("ocultar");
+
+});
+
+/* FIN */
+
+
+/* side menu left */
+
+const btn = document.querySelector('#menu-btn');
+const menu = document.querySelector('#sidemenu');
+
+btn.addEventListener('click', e =>{
+    menu.classList.toggle("menuexpanded");
+    menu.classList.toggle("menucolapse");
+
+    document.querySelector('body').classList.toggle('body-expanded');
+
+});
+/* end side menu */
+
+
+
+
+
+
+
+
+/* fin modal tabla */
+
