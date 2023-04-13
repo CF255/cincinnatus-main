39dea8696b2583ee04a46a5180e7ebb7fb9a9c02
andrews fer <andrewsluis2011@gmail.com>
2023-04-13 17:05:39 -0400

    rf

--- server.js
+++ server.js
@@ -1,15 +1,15 @@
 const express = require("express");
 const app = express();
+const {pool} = require("./dbconfig");
 
 
 const PORT = process.env.PORT || 4000;
 
 app.set("view engine", "ejs");
+app.use(express.urlencoded({extended: false}));
 
 app.get("/",(req, res)=>{
     res.render("index");
-
-
 });
 
 app.get("/users/registro",(req,res)=>{
@@ -24,11 +24,44 @@ app.get("/users/dashboard",(req,res)=>{
         res.render("dashboard" , {usuario: "andrews"});
         });
 
+app.post("/users/registro",(req,res)=>{
+    let{usuario,nombre,apellido,fecha,email,rol,password,password2} = req.body;
+    console.log({
+        usuario,
+        nombre,
+        apellido,
+        fecha,
+        email,
+        rol,
+        password,
+        password2,
+    });
+
+    let errors = [];
+
+    if(!usuario || !nombre || !apellido || !fecha || !email || !rol || !password ||!password2){
+        errors.push({message: "Completar todos los campos"});
+    }
+
+    if(password.length < 6){
+        errors.push({message: "La contraseña debe de tener al menos 6 caracteres"});
+    }
+
+    if(password != password2){
+        errors.push({message: "Las contraseñas no coinciden"});
+    }
+
+    if(errors.length > 0){
+        res.render("registro", {errors});
+        
+    }
+});
+
 app.listen(PORT,()=>{
     console.log(`server running on port ${PORT}`);
 
 
-})
+});
 
 
 
