fa24777c83817d5e3705a11ea33eaea650a36a8e
CF255 <andrewsluis2011@gmail.com>
2023-04-19 14:30:08 -0400

    cambios

--- passportconfig.js
+++ passportconfig.js
@@ -16,7 +16,6 @@ function initialize(passport){
                                 throw err;
                             }
                             console.log(results.rows);
-            
                             if(results.rows.length > 0 ){
                                 const user = results.rows[0];
                             bcrypt.compare(pass, user.pass,(err, isMatch)=>{
