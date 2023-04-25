5d507aed080eb1f69b616aabae4ab1e3bded556e
andrews fer <andrewsluis2011@gmail.com>
2023-04-25 09:09:44 -0400

    optimizacion css

--- app/views/adminin.php
+++ app/views/adminin.php
@@ -10,13 +10,13 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Registro de productos</title>
     <link rel="stylesheet" href="../../public/css/adminin.css?v=<?php echo(rand()); ?>" />
+    <link rel="stylesheet" href="../../public/css/menus.css?v=<?php echo(rand()); ?>" />
 
   <!--   <link rel="stylesheet" href="../../public/css/almacen.css">
     --> <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link href="https://fonts.googleapis.com/css2?family=Paytone+One&display=swap" rel="stylesheet">
    
  
-
 
 </head>
 <body>
